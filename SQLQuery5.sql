8f4a51532da2667bac05d601d02a7c3bea179985
DELL <DELL@RDUESSHKITOP>
2024-04-06 22:10:36 +0530

    Add project files.

---
+++ SQLQuery5.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE addPatient(
+
+ id INT PRIMARY KEY IDENTITY(1,1),
+ patient_name VARCHAR(MAX) NULL,
+ doctor VARCHAR(MAX) NULL,
+ registered DATE NULL,
+ status VARCHAR(MAX) NULL,
+ date_insert DATE NULL,
+ date_update DATE NULL,
+ date_delete DATE NULL
+
+)
+SELECT * FROM addPatient
